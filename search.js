16821bc741d9fbe8efcae371385a804e700ed07e
Kevin <kevin78515@gmail.com>
2015-04-30 19:36:57 +0800

    answer

--- search.js
+++ search.js
@@ -4,10 +4,18 @@ var word = process.argv[2], // 要找的字
 	file = process.argv[3], // 被搜尋的檔案
 	number = 0; 
 
-console.log('Start serach \"'+word+'" in "'+file+'"...')
+console.log('Start serach \"'+word+'" in "'+file+'"...');
 
 // === 開始寫程式吧 === //
 
+fs.readFile(file, "utf8", function(err, data){
+	var words = data.split(" ");
+	for(var i=0; i<words.length; i++){
+		if(words[i]==word){
+			number++;
+		}
+	}
+	console.log("There's "+number+" \""+word+"\" in \""+file+"\"!");
+});
 
 
-console.log("There's "+number+" \""+word+"\" in \""+file+"\"!");
